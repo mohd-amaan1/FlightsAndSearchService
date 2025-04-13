f958e8c0c8b7bf8e7972f9e9b4bc6b20eb4e45b2
mohdamaan069 <118156938+mohdamaan069@users.noreply.github.com>
2025-04-13 19:29:57 +0530

    added server config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
-const app = express();
+const PORT = require("./config/serverConfig").PORT;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const startAndSetupServer = async () => {
+  const app = express();
 
-require("dotenv").config();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+startAndSetupServer();
